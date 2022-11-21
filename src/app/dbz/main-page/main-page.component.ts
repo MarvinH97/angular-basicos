86629fae4a2e36b656e1305b570a0712c9cc633c
Marvin Hernández <marvin.geovany1997@gmail.com>
2022-11-21 13:52:41 -0600

    Se subieron demás archivos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  constructor() { }
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
 
-  ngOnInit(): void {
-  }
+  constructor(){}
 
 }
